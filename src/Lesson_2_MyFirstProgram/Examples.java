05e89ab7bc9538981c5d14813b52b971cf1be119
emrekcmn <emre.kcmn15@gmail.com>
2023-02-18 20:39:43 +0300

    added Lesson_2

---
+++ src/Lesson_2_MyFirstProgram/Examples.java
@@ -0,0 +1,23 @@
+package Lesson_2_MyFirstProgram;
+
+public class Examples {
+    public static void main(String[] args) {
+
+        System.out.println("To be, or not to be: that is the question.");
+        System.out.println();
+        System.out.println("Hiç kimse başarı merdivenlerini elleri ");
+        System.out.print("cebinde tırmanmamıştır.");
+
+        System.out.println();
+        System.out.println("  ************");
+        System.out.println("         **");
+        System.out.println("         **");
+        System.out.println("         **");
+        System.out.println("         **");
+        System.out.println("**       **");
+        System.out.println(" **      **");
+        System.out.println("  **    **");
+        System.out.println("    ****");
+
+    }
+}
---
+++ src/Lesson_2_MyFirstProgram/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package Lesson_2_MyFirstProgram;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println("HELLO");
+        System.out.println("WORLD");
+        System.out.println();
+        System.out.print("HELLO WORLD");
+
+    }
+}
