90a0476327f1cc2569bb7542d0015eb47b9bf493
emrekcmn <emre.kcmn15@gmail.com>
2023-02-18 20:34:43 +0300

    added Lesson_1

---
+++ src/Lesson_1_What_Is_Java/Beginnig.java
@@ -0,0 +1,42 @@
+package Lesson_1_What_Is_Java;
+
+public class Beginnig {
+    public static void main(String[] args) {
+
+       /*
+         Java is an object-oriented, class-based, concurrent,
+         secured and general-purpose computer-programming language.
+    > java fundamentals coming from c++ // javanın temeli c++ dilinden gelmektedir
+    >java simple learn/secure/high level/high performance/dynamic....programing language
+    >java know "platform independent programing language"---write anywhere run everywhere
+                                    //herhangi bir platformda yaz istediğin platformda run et
+
+                                    JAVA COMPONENTS(java bölümleri)
+                                    -----------------------
+                                    |     JDK             |         java development kit
+                                    |     ----------------|
+                                    |     |     JRE       |         java runtime environment
+                                    |     |    -----------|
+                                    |     |    |    JVM   |         java virtual machine provide platform independence
+                                    |     |    |          |
+                                    ----------------------
+
+
+
+              //binary
+        //0 üstlü sayısı için u+2070
+        //1 üstlü sayısı için ALT+0185
+        //2 üstlü sayısı için ALT+0178
+        //3 üstlü sayısı için ALT+0179
+        //4 üstlü sayısı için u+2074
+        //5 üstlü sayısı için u+2075
+        //6 üstlü sayısı için u+2076
+        //7 üstlü sayısı için u+2077
+        //8 üstlü sayısı için u+2078
+        //9 üstlü sayısı için u+2079
+
+
+
+     */
+    }
+}
